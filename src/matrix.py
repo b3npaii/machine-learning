3624e6ae5e4e0f1060cf49c251471aa66f605d54
Benjamin Park <89424717+b3npaii@users.noreply.github.com>
2021-10-07 21:48:33 +0000

    determinant, rref, helpers, clean stuff

---
+++ src/matrix.py
@@ -0,0 +1,237 @@
+class Matrix:
+    def __init__(self, rows):
+        self.rows = rows
+        self.num_rows = len(rows)
+        self.num_cols = len(rows[0])
+
+    def print(self):
+        for row in self.rows:
+            print(row)
+
+    def copy(self):
+        copied_matrix = [[element for element in row] for row in self.rows]
+        return Matrix(copied_matrix)
+
+    def add(self, other_matrix):
+        answer = []
+        for i in range(0, self.num_rows):
+            answer.append([])
+            for j in range(0, self.num_cols):
+                answer[i].append(self.rows[i][j] + other_matrix.rows[i][j])
+        return Matrix(answer)
+
+    def subtract(self, other_matrix):
+        new_rows = other_matrix.rows
+        result = []
+        for row in self.rows:
+            result.append([])
+        for i in range(0, self.num_rows):
+            for j in range(0, self.num_cols):
+                result[i].append(self.rows[i][j] - new_rows[i][j])
+        return Matrix(result)
+
+    def transpose(self):
+        if self.num_cols == self.num_rows:
+            transposed_rows = []
+            for i in range(0, self.num_rows):
+                transposed_rows.append([])
+                for j in range(0, self.num_cols):
+                    transposed_rows[i].append(self.rows[j][i])
+            return Matrix(transposed_rows)
+        else:
+            transposed_rows = []
+            for i in range(0, self.num_cols):
+                transposed_rows.append([])
+                for j in range(0, self.num_rows):
+                    transposed_rows[i].append(self.rows[j][i])
+            return Matrix(transposed_rows)
+
+    def scalar_multiply(self, scalar):
+        rescaled_rows = []
+        for row in self.rows:
+            rescaled_rows.append([])
+        for i in range(0, self.num_cols):
+            for j in range(0, self.num_rows):
+                rescaled_rows[i].append(self.rows[j][i] * scalar)
+        return Matrix(rescaled_rows)
+
+    def matrix_multiplication(self, other_matrix):
+        product = []
+        for i in range(self.num_rows):
+            product.append([])
+            for j in range(other_matrix.num_cols):
+                product[i].append(0)
+        for i in range(self.num_rows):
+            for j in range(other_matrix.num_cols):
+                for k in range(self.num_cols):
+                    product[i][j] += self.rows[i][k] * other_matrix.rows[k][j]
+        return Matrix(product)
+
+    def trim_matrix(self, i):
+        trimmed_rows = self.copy().rows
+        del trimmed_rows[0]
+        for row in trimmed_rows:
+            del trimmed_rows[trimmed_rows.index(row)][i]
+        return Matrix(trimmed_rows)
+
+    def recursive_determinant_calc(self):
+        #print("taking the determinant of:")
+        #self.print()
+        #print("this matrix has ", self.num_cols, "columns")
+        #print("this matrix has ", self.num_rows, "rows")
+        if self.num_cols != self.num_rows:
+            return "cannot take a determinant"
+
+        elif self.num_rows == 2 and self.num_cols == 2:
+            #print("got to the base case")
+            det = (self.rows[0][0] * self.rows[1][1]) - (self.rows[0][1] * self.rows[1][0])
+            return det
+
+        else:
+            determinant = 0
+            for i in range(0, self.num_cols):
+                #print("trimming the following matrix:")
+                #self.print()
+                coefficient = self.rows[0][i] * ((-1) ** i)
+                smaller_matrix = self.trim_matrix(i)
+                #print("coefficient = ", coefficient)
+                #print("the smaller matrix is")
+                #smaller_matrix.print()
+                cofactor = coefficient * smaller_matrix.recursive_determinant_calc()
+                determinant += cofactor
+            return determinant
+
+    def first_nonzero(self, i):
+        for j in range(0, self.num_cols):
+            if self.rows[i][j] != 0:
+                return j
+            elif j == self.num_cols - 1:
+                break
+
+    def check_pivot(self, i):
+        for j in range(0, self.num_rows):
+            if self.rows[i][j] != 0:
+                if self.rows[i][:] == 0:
+                    break
+                else:
+                    return j
+            elif j == self.num_rows:
+                return False
+
+    def add_rows(self, i, j, scalar):
+        added_row = self.copy().rows
+        for k in range(0, self.num_cols):
+            added_row[i][k] += scalar * self.rows[j][k]
+        return Matrix(added_row)
+
+    def row_swap(self, i, j):
+        swapped_rows = self.copy().rows
+        swapped_rows[i], swapped_rows[j] = swapped_rows[j], swapped_rows[i]
+        return Matrix(swapped_rows)
+
+    def rescale_row_by_element_of_row(self, i, j):
+        changed_rows = self.copy().rows
+        for k in range(0, self.num_cols):
+            changed_rows[i][k] = (1 / self.rows[i][j]) * self.rows[i][k]
+        return Matrix(changed_rows)
+
+    def clear_above(self, i):
+        cleared_rows = self.copy().rows
+        nonzero = self.first_nonzero(i)
+        for j in range(0, i):
+            scalar = -1 * cleared_rows[j][nonzero]/cleared_rows[i][nonzero]
+            cleared_matrix = Matrix(cleared_rows)
+            cleared_matrix = cleared_matrix.add_rows(j, i, scalar)
+            cleared_rows = cleared_matrix.rows
+        return Matrix(cleared_rows)
+
+    def clear_below(self, i):
+        cleared_rows = self.copy().rows
+        nonzero = self.first_nonzero(i)
+        if len(cleared_rows) < len(cleared_rows[0]):
+            for j in range(i + 1, self.num_cols - 1):
+                scalar = -1 * cleared_rows[j][nonzero]/cleared_rows[i][nonzero]
+                cleared_matrix = Matrix(cleared_rows)
+                cleared_matrix = cleared_matrix.add_rows(j, i, scalar)
+                cleared_rows = cleared_matrix.rows
+        else:
+            for j in range(i + 1, self.num_cols):
+                scalar = -1 * cleared_rows[j][nonzero]/cleared_rows[i][nonzero]
+                cleared_matrix = Matrix(cleared_rows)
+                cleared_matrix = cleared_matrix.add_rows(j, i, scalar)
+                cleared_rows = cleared_matrix.rows
+        return Matrix(cleared_rows)
+
+    def first_nonzero_element_is_1(self, i):
+        j = self.first_nonzero(i)
+        copy = self.copy().rows
+        for k in range(0, self.num_cols):
+            copy[i][k] /= self.rows[i][j]
+        return Matrix(copy)
+
+    def check_if_zero_row(self, i):
+        counter = 0
+        for element in self.rows[i]:
+            if element == 0:
+                counter += 1
+        if counter == self.num_cols:
+            return True
+        else:
+            return False
+
+    def check_if_reduced_row(self, i):
+        counter = 0
+        for element in self.rows[i]:
+            if element == 0 or element == 1:
+                counter += 1
+        if counter == self.num_cols:
+            return True
+        else:
+            return False
+
+    def rref(self):
+        copied_matrix = self.copy()
+        if copied_matrix.num_cols > copied_matrix.num_rows:
+            for i in range(0, copied_matrix.num_rows):
+                if copied_matrix.check_if_zero_row(i) == True:
+                    copied_matrix = copied_matrix.row_swap(i, self.num_rows - 1)
+                    continue
+                copied_matrix = copied_matrix.first_nonzero_element_is_1(i)
+                copied_matrix = copied_matrix.clear_above(i)
+                copied_matrix = copied_matrix.clear_below(i)
+            for i in range(0, copied_matrix.num_rows - 1):
+                if copied_matrix.check_if_zero_row(i) == True or copied_matrix.check_if_zero_row(i + 1) == True:
+                    continue
+                elif copied_matrix.first_nonzero(i) > copied_matrix.first_nonzero(i + 1):
+                    copied_matrix = copied_matrix.row_swap(i, i + 1)
+        elif copied_matrix.num_cols == copied_matrix.num_rows:
+            for j in range(0, copied_matrix.num_rows):
+                for i in range(0, copied_matrix.num_rows):
+                    if copied_matrix.check_if_reduced_row(i) == True:
+                        continue
+                    if copied_matrix.check_if_zero_row(i) == True:
+                        copied_matrix = copied_matrix.row_swap(i, self.num_cols)
+                        continue
+                    copied_matrix = copied_matrix.first_nonzero_element_is_1(i)
+                    copied_matrix = copied_matrix.clear_above(i)
+                    copied_matrix = copied_matrix.clear_below(i)
+            for i in range(0, copied_matrix.num_rows - 1):
+                if copied_matrix.check_if_zero_row(i) == True or copied_matrix.check_if_zero_row(i + 1) == True:
+                    continue
+                elif copied_matrix.first_nonzero(i) > copied_matrix.first_nonzero(i + 1):
+                    copied_matrix = copied_matrix.row_swap(i, i + 1)
+        elif copied_matrix.num_cols < copied_matrix.num_rows:
+            for j in range(0, copied_matrix.num_rows):
+                for i in range(0, copied_matrix.num_rows):
+                    if copied_matrix.check_if_zero_row(i) == True:
+                        copied_matrix = copied_matrix.row_swap(i, self.num_cols)
+                        continue
+                    copied_matrix = copied_matrix.first_nonzero_element_is_1(i)
+                    copied_matrix = copied_matrix.clear_above(i)
+                    copied_matrix = copied_matrix.clear_below(i)
+            for i in range(0, copied_matrix.num_rows - 1):
+                if copied_matrix.check_if_zero_row(i) == True or copied_matrix.check_if_zero_row(i + 1) == True:
+                    continue
+                elif copied_matrix.first_nonzero(i) > copied_matrix.first_nonzero(i + 1):
+                    copied_matrix = copied_matrix.row_swap(i, i + 1)
+        return copied_matrix
---
+++ tests/test_matrix.py
@@ -0,0 +1,52 @@
+import sys
+sys.path.append('src')
+
+from matrix import Matrix
+
+A = Matrix([[4, 3], [2, 1]])
+B = Matrix([[4, 3], [2, 1]])
+Identity = Matrix([[1, 0], [0, 1]])
+A_and_identity = Matrix([[5, 3], [2, 2]])
+At = Matrix([[4, 2], [3, 1]])
+Ad = Matrix([[1, 1], [-1, -1]])
+double_identity = Matrix([[2, 0], [0, 2]])
+Three_Identity = Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+if A.transpose().rows != At.rows:
+    print("Transpose failed")
+    A.transpose().print()
+if A.add(Identity).rows != A_and_identity.rows:
+    print("addition failed")
+    A.add(Identity).print()
+if A_and_identity.subtract(Identity).rows != A.rows:
+    print("subtraction failed")
+if Identity.scalar_multiply(2).rows != double_identity.rows:
+    print("scalar_multiply failed")
+    Identity.scalar_multiply(2).print()
+C = Matrix([[4, 7], [0, -3], [9, 1]])
+D = Matrix([[1, 2, -1], [1, 0, 1]])
+Ay = Matrix([[7, -1, 11, 0], [8, -2, 3, 1], [-5, 4, -2, 6], [0, 5, -8, -9]])
+X = Matrix([[3, 0, 0, 2], [8, 0, 3, 9], [2, 0, 2, 2], [1, 0, 1, 1]])
+if Ay.recursive_determinant_calc() != -4352:
+    print("recursive_determinant_calc is broken")
+if B.recursive_determinant_calc() != -2:
+    print("recursive_determinant_calc is broken")
+print("The row reduced echelon form of")
+D.print()
+print("is:")
+D.rref().print()
+print()
+print("The row reduced echelon form of")
+Ay.print()
+print("is:")
+Ay.rref().print()
+print()
+print("The row reduced echelon form of")
+C.print()
+print("is:")
+C.rref().print()
+print()
+print("The row reduced echelon form of")
+X.print()
+print("is:")
+X.rref().print()
