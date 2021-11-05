59acc1dfc2bca25814559f0c7b70aaa6c5d7d05e
b3npaii <benjaminmpark23@gmail.com>
2021-11-05 22:25:33 +0000

    better sandwiches

--- src/analysis.py
+++ src/analysis.py
@@ -30,4 +30,42 @@ class LinearRegression:
 
 a = LinearRegression()
 a.fit([[0, 0, 1], [1, 0, 2], [2, 0, 4], [4, 0, 8], [6, 0, 9], [0, 2, 2], [0, 4, 5], [0, 6, 7], [0, 8, 6]])
-print(a.predict(5, 5))
+print(a.predict(5, 0))
+
+class BetterSandwich:
+    def fit(self, points):
+
+        self.data = points
+        for point in self.data: 
+            point.append(point[0] * point[1])
+        length = len(self.data[0])
+        rows = [[point[-1]] for point in points]
+        y_matrix = Matrix(rows)
+
+        coefficient_rows = [[1] for point in points]
+        for i in range(0, len(self.data)):
+            for j in range(0, length - 1):
+                coefficient_rows[i].append(self.data[i][j])
+        
+        coefficient_matrix = Matrix(coefficient_rows)
+
+        coefficient_transpose = coefficient_matrix.transpose()
+        transpose_times_y = coefficient_transpose.matrix_multiplication(y_matrix)
+        transpose_times_coefficients = coefficient_transpose.matrix_multiplication(coefficient_matrix)
+
+        inverse_matrix = transpose_times_coefficients.inverse()
+        a_b_matrix = inverse_matrix.matrix_multiplication(transpose_times_y)
+        self.coefficients = []
+        for i in range(a_b_matrix.num_rows):
+            self.coefficients.append(a_b_matrix.rows[i][0])
+
+    def predict(self, inputs):
+        answer = 0
+        numbers = self.coefficients
+        for i in range(1, len(inputs)):
+            answer += inputs[i - 1] * numbers[i]
+        answer += self.coefficients[0]
+        return answer
+b = BetterSandwich()
+b.fit([[0, 0, 1], [1, 0, 2], [2, 0, 4], [4, 0, 8], [6, 0, 9], [0, 2, 2], [0, 4, 5], [0, 6, 7], [0, 8, 6]])
+print(b.predict([5, 0]))
